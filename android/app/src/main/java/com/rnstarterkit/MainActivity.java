6685877046ff8de04ca7a0c9d1ce0bacf6b789ae
duyanhv <duyanhv187@gmail.com>
2023-10-03 21:18:08 +0700

    upgrade dependencies android project setting

--- android/app/src/main/java/com/rnstarterkit/MainActivity.java
+++ android/app/src/main/java/com/rnstarterkit/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends ReactActivity {
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
         // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
-        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+        // DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
         );
   }
 }
