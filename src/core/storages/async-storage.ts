2b8ec355d8f881bb7c240ef72ee5b8ffaa7c44ab
duyanhv <duyanhv187@gmail.com>
2023-10-04 21:20:38 +0700

    replace asyncstorage with mmkv

--- src/core/storages/async-storage.ts
+++ src/core/storages/async-storage.ts
@@ -1,36 +1,36 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import {MMKV} from 'react-native-mmkv';
+export const _storage = new MMKV();
 const get = async <T = string | Object>(key: string, needDeserialized: boolean = true): Promise<T | undefined> => {
-	try {
-		const value = await AsyncStorage.getItem(key);
-		if (!value) {
-			return undefined;
-		}
-		return value && needDeserialized ? JSON.parse(value) : value;
-	} catch (e) {
-		// saving error
-		return undefined;
-	}
+  try {
+    const value = _storage.getString(key);
+    if (!value) {
+      return undefined;
+    }
+    return value && needDeserialized ? JSON.parse(value as string) : value;
+  } catch (e) {
+    // saving error
+    return undefined;
+  }
 };
 
 const set = async <T = string | Object>(key: string, value: T): Promise<void> => {
-	try {
-		await AsyncStorage.setItem(key, typeof value === 'string' ? value : JSON.stringify(value));
-	} catch (e) {
-		// getting error
-	}
+  try {
+    _storage.set(key, typeof value === 'string' ? value : JSON.stringify(value));
+  } catch (e) {
+    // getting error
+  }
 };
 
 const remove = async (key: string): Promise<void> => {
-	try {
-		await AsyncStorage.removeItem(key);
-	} catch (e) {
-		// getting error
-	}
+  try {
+    _storage.delete(key);
+  } catch (e) {
+    // getting error
+  }
 };
 
 export const storage = {
-	get,
-	set,
-	remove,
+  get,
+  set,
+  remove,
 };
